6e4f074cf9ddda51faeda5c1ddadac00d0c7f8a6
brakdag <brakdag@gmail.com>
2020-12-27 17:29:45 -0300

    example

--- test.js
+++ test.js
@@ -2,6 +2,19 @@
 // Random test.
 
 var rand = require('./index.js')
+var j=rand.random() // load init table
+const MAX_STRESS = 1000000
+x = []
+y = []
 
-for(i=0;i<100;i++) 
-	console.log(rand.random())
+console.time("testing fastrandom")
+for(i=0;i<MAX_STRESS;i++){ 
+	x.push(rand.random())
+}
+console.timeEnd("testing fastrandom")
+
+console.time("testing Math.random")
+for(i=0;i<MAX_STRESS;i++){ 
+	y.push(Math.random())
+}
+console.timeEnd("testing Math.random")
